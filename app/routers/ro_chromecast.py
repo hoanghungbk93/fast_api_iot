de24ea5f071dd74ea7bc56f1945a0e68367d47eb
hoanghungbk93 <hch.bkhn@gmail.com>
2025-02-28 22:36:33 +0700

    add logfire and sendtry to env

--- app/routers/ro_chromecast.py
+++ app/routers/ro_chromecast.py
@@ -7,12 +7,12 @@ from typing import List
 
 router = APIRouter(prefix="/chromecasts", tags=["chromecasts"])
 
-@router.get("/chromecasts/", response_model=List[ChromecastSchema])
+@router.get("/chromecasts", response_model=List[ChromecastSchema])
 def read_chromecasts(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     chromecasts = db.query(Chromecast).offset(skip).limit(limit).all()
     return chromecasts
 
-@router.post("/chromecasts/", response_model=ChromecastSchema)
+@router.post("/chromecasts", response_model=ChromecastSchema)
 def create_chromecast(chromecast: ChromecastCreate, db: Session = Depends(get_db)):
     db_chromecast = Chromecast(**chromecast.dict())
     db.add(db_chromecast)
