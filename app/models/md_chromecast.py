61323d51e7b009cdc3e515a60dc87a77c1b4a805
hoanghungbk93 <hch.bkhn@gmail.com>
2025-03-02 20:01:41 +0700

    fi

--- app/models/md_chromecast.py
+++ app/models/md_chromecast.py
@@ -7,7 +7,7 @@ class Chromecast(Base):
     id = Column(Integer, primary_key=True, index=True)
     code = Column(String, unique=True, index=True, nullable=False)
     mac_address = Column(String, nullable=False)
-    uuid = Column(String, nullable=False)
+    uuid = Column(String, nullable=True)
 
     # Define the relationship to Pair
     pairs = relationship("Pair", back_populates="chromecast")
