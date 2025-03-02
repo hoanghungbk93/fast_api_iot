adb1f98379aba246595686bd7e05edcef019839d
hoanghungbk93 <hch.bkhn@gmail.com>
2025-03-02 17:46:30 +0700

    .

--- app/routers/ro_chromecast.py
+++ app/routers/ro_chromecast.py
@@ -44,26 +44,8 @@ async def checkout(chromecast_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Chromecast not found")
 
     # Lấy IP từ MAC Address
-    chromecast_ip = get_ip_from_mac(chromecast.mac_address)
-
-    # Sử dụng CastBrowser để tìm Chromecast
-    cast_listener = pychromecast.discovery.SimpleCastListener()
-
-    browser = pychromecast.discovery.CastBrowser(cast_listener)
-    browser.start_discovery()
-    
-    time.sleep(3)  # Chờ quét Chromecast
-    browser.stop_discovery()
-
-    
-    # Lấy danh sách các thiết bị tìm được
-    devices = cast_listener.devices
-    chromecast_device = next((dev for dev in devices if dev.host == chromecast_ip), None)
-
-    if not chromecast_device:
-        raise HTTPException(status_code=404, detail="Chromecast not found on network")
-
-    cast = pychromecast.get_chromecast_from_host((chromecast_ip, 8009))
+    chromecast_ip = get_ip_from_mac(chromecast.mac_address) 
+    cast = pychromecast.Chromecast(chromecast_ip)
     cast.wait()
 
     # Ngắt ứng dụng hiện tại
@@ -80,5 +62,4 @@ async def checkout(chromecast_id: int, db: Session = Depends(get_db)):
     except subprocess.CalledProcessError as e:
         raise HTTPException(status_code=500, detail=f"ADB command failed: {e}")
 
-    browser.stop_discovery()
     return {"message": f"Checkout initiated for Chromecast {chromecast.code} at {chromecast_ip}"}
