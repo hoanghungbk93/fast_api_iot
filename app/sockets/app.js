76b9255b0aa81e0ad16b30c6cdcd8f9f3627570a
hoanghungbk93 <hch.bkhn@gmail.com>
2025-03-07 19:23:35 +0700

    fix

--- app/sockets/app.js
+++ app/sockets/app.js
@@ -368,8 +368,8 @@ function sendCastCommand(ip, command, callback) {
                     return callback(err);
                 }
 
-                const receiverController = receiver.createController('ReceiverController');
-                receiverController.send({
+                const platform = receiver.createController('PlatformSender');
+                platform.send({
                     type: "KEYPRESS",
                     key: commands[command]
                 }, (err) => {
